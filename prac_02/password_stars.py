204946078a7af49696dbaa94e1eff365e5c3fda8
Liam EIme <liameime@outlook.com>
2022-09-22 16:28:33 +1000

    Add password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -3,3 +3,9 @@ CP1404 | Practical 01 - menus | Liam Eime
 Program to ...
 """
 
+password = input("Enter password: ")
+minimum_password_length = 5
+while len(password) < minimum_password_length:
+    print("Password too short, must be at least {} characters".format(minimum_password_length))
+    password = input("Enter password: ")
+print('*' * len(password))
