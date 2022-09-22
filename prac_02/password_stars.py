aae9586240189808e1c952c6e332549155f7e6d0
Liam EIme <liameime@outlook.com>
2022-09-22 16:57:57 +1000

    Refactor password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,16 +1,27 @@
 """
 CP1404 | Practical 01 - menus | Liam Eime
-Program to ...
+Program to get password with error checking
 """
 
 
 def main():
     """Main function"""
-    password = input("Enter password: ")
     minimum_password_length = 5
+    password = get_password(minimum_password_length)
+    print_asterisks(password)
+
+
+def get_password(minimum_password_length):
+    """Get password and check if it is a valid length"""
+    password = input("Enter password: ")
     while len(password) < minimum_password_length:
         print("Password too short, must be at least {} characters".format(minimum_password_length))
         password = input("Enter password: ")
+    return password
+
+
+def print_asterisks(password):
+    """Print a number of asterisks equal to the length of the password"""
     print('*' * len(password))
 
 
