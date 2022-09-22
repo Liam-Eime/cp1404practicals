213df4697171932c06968b93b98cd43176f1a8d6
Liam EIme <liameime@outlook.com>
2022-09-22 16:38:20 +1000

    Adding main()

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -3,9 +3,15 @@ CP1404 | Practical 01 - menus | Liam Eime
 Program to ...
 """
 
-password = input("Enter password: ")
-minimum_password_length = 5
-while len(password) < minimum_password_length:
-    print("Password too short, must be at least {} characters".format(minimum_password_length))
+
+def main():
+    """Main function"""
     password = input("Enter password: ")
-print('*' * len(password))
+    minimum_password_length = 5
+    while len(password) < minimum_password_length:
+        print("Password too short, must be at least {} characters".format(minimum_password_length))
+        password = input("Enter password: ")
+    print('*' * len(password))
+
+
+main()
