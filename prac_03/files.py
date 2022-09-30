c088f819e167dcf8bf9e739689b9d906999474c4
Liam EIme <liameime@outlook.com>
2022-09-30 15:15:02 +1000

    read first two numbers in number.txt and add them

--- prac_03/files.py
+++ prac_03/files.py
@@ -6,10 +6,16 @@ FILE_NAME = "name.txt"
 
 name = input("Enter name: ")
 name_text = open(FILE_NAME, 'w')
-print("{}".format(name), file=name_text)
+print(f"{name}", file=name_text)
 name_text.close()
 
 name_text = open(FILE_NAME, 'r')
 text = name_text.read()
 print(f"Your name is {text}")
 name_text.close()
+
+numbers_text = open("numbers.txt", 'r')
+first_number = int(numbers_text.readline())
+second_number = int(numbers_text.readline())
+print(f"Total of first two numbers if {first_number + second_number}")
+numbers_text.close()
