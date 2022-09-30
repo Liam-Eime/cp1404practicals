4de2d6ba79da2c7928393056cd6db364351d315a
Liam EIme <liameime@outlook.com>
2022-09-30 15:25:11 +1000

    update to use with open for simplification

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,18 +4,15 @@ Program to read and write to files
 """
 FILE_NAME = "name.txt"
 
-name = input("Enter name: ")
-name_text = open(FILE_NAME, 'w')
-print(f"{name}", file=name_text)
-name_text.close()
+with open(FILE_NAME, "w") as name_text:
+    name = input("Enter name: ")
+    print(f"{name}", file=name_text)
 
-name_text = open(FILE_NAME, 'r')
-text = name_text.read()
-print(f"Your name is {text}")
-name_text.close()
+with open(FILE_NAME, "r") as name_text:
+    text = name_text.read()
+    print(f"Your name is {text}")
 
-numbers_text = open("numbers.txt", 'r')
-first_number = int(numbers_text.readline())
-second_number = int(numbers_text.readline())
-print(f"Total of first two numbers if {first_number + second_number}")
-numbers_text.close()
+with open("numbers.txt", "r") as numbers_text:
+    first_number = int(numbers_text.readline())
+    second_number = int(numbers_text.readline())
+    print(f"Total of first two numbers if {first_number + second_number}")
