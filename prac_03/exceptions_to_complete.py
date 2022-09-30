8acf3e38aecb78ef24a0648eba042cc6d8f93c55
Liam EIme <liameime@outlook.com>
2022-09-30 14:12:22 +1000

    complete ValueError catch for exceptions_to_complete.py

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -1,5 +1,13 @@
 """
 CP1404 | Practical 03 - exceptions_to_complete  | Liam Eime
-Program to ...
+Program to get integer from user and not crash when invalid input
 """
 
+is_finished = False
+while not is_finished:
+    try:
+        result = int(input("Enter a valid integer: "))
+        is_finished = True
+    except ValueError:
+        print("Please enter a valid integer.")
+print("Valid result is:", result)  # safe to "may be undefined" for result
