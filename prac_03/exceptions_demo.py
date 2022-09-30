3223edf3c7d02bc98163f9a21b2f2e6b1b95912e
Liam EIme <liameime@outlook.com>
2022-09-30 14:00:52 +1000

    change exceptions_demo.py to avoid possibility of a ZeroDivisionError

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -6,16 +6,16 @@ Answer the following questions:
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
 """
 
-try:
-    numerator = int(input("Enter the numerator: "))
-    denominator = int(input("Enter the denominator: "))
-    fraction = numerator / denominator
-    print(fraction)
-except ValueError:
-    print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
-print("Finished.")
+numerator = int(input("Enter the numerator: "))
+denominator = int(input("Enter the denominator: "))
+while denominator != 0:
+    try:
+        fraction = numerator / denominator
+        print(fraction)
+    except ValueError:
+        print("Numerator and denominator must be valid numbers!")
+    print("Finished.")
+print("Cannot divide by zero!")
 
 # ValueError will occur when either numerator and/or denominator are not numbers
 # ZeroDivisionError will occur when the denominator is zero and numerator is a valid number
