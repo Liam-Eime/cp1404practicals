6b3f9d783aafa48fbb1062ccd5df44398cffc236
Liam EIme <liameime@outlook.com>
2022-09-30 15:01:53 +1000

    write to file called name.txt and read from file name.txt

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,5 +1,15 @@
 """
 CP1404 | Practical 03 - files  | Liam Eime
-Program to ...
+Program to read and write to files
 """
+FILE_NAME = "name.txt"
 
+name = input("Enter name: ")
+name_text = open(FILE_NAME, 'w')
+print("{}".format(name), file=name_text)
+name_text.close()
+
+name_text = open(FILE_NAME, 'r')
+text = name_text.read()
+print(f"Your name is {text}")
+name_text.close()
