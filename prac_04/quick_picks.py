23a4d7838dfb3b7dd23ff48ad5620d583bd3c113
Liam EIme <liameime@outlook.com>
2022-10-07 14:47:12 +1000

    make MINIMUM and MAXIMUM constant for random number generator in quick_picks.py

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -5,13 +5,15 @@ Program to ...
 import random
 
 LENGTH_OF_QUICK_PICK = 6
+MINIMUM = 1
+MAXIMUM = 45
 
 number_of_quick_picks = int(input("How many quick picks? "))
 
 for i in range(number_of_quick_picks):
     quick_pick = []
     for j in range(LENGTH_OF_QUICK_PICK):
-        random_number = random.randint(1, 45)
+        random_number = random.randint(MINIMUM, MAXIMUM)
         quick_pick.append(random_number)
         quick_pick.sort()
     print(quick_pick)
