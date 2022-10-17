49b36a1b054ba14365eb03e4c613fa64bc1fc548
Liam EIme <liameime@outlook.com>
2022-10-17 16:41:07 +1000

    Complete word_occurrences.py for prac_05 task

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,13 +2,16 @@
 CP1404 | Practical 05 - word_occurrences  | Liam Eime
 Program to count the occurrences of words in a string
 Estimate: 30 minutes
-Actual:
+Actual: 26.5 minutes
 """
 
 string = str(input("Enter a string of words: "))
 word_to_count = {}
+
 for word in string.split(" "):
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
+max_length = max((len(word) for word in list(word_to_count.keys())))
+
 for word in sorted(word_to_count):
-    print(f"{word} : {word_to_count[word]}")
+    print(f"{word:{max_length}} : {word_to_count[word]}")
