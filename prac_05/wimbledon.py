e40eea1a7ea090b76710d7a13c46bc737afec820
Liam EIme <liameime@outlook.com>
2022-10-18 08:07:41 +1000

    Read wimbledon.csv into wimbledon.py and store data as list of lists

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -5,3 +5,16 @@ Estimate: 60 minutes
 Actual:
 """
 
+FILENAME = "wimbledon.csv"
+wimbledon_champions = []
+
+
+def main():
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()  # read first line to remove header
+        for line in in_file:
+            wimbledon_champions.append(line.split(","))
+        print(wimbledon_champions)
+
+
+main()
