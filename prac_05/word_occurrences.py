12975c470a7c89e1d8a276575d8957341377fd43
Liam EIme <liameime@outlook.com>
2022-10-17 16:13:37 +1000

    Count occurrence for each word in string for word_occurrences.py

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -4,3 +4,8 @@ Program to count the occurrences of words in a string
 Estimate: 30 minutes
 Actual:
 """
+
+string = str(input("Enter a string of words: "))
+word_to_count = {}
+for word in string.split(" "):
+    word_to_count[word] = word_to_count.get(word, 0) + 1
