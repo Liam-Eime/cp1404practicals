f1f672599c571848183d2da3421baf1783b7a567
Liam EIme <liameime@outlook.com>
2022-10-17 16:23:14 +1000

    Sort words in word_to_count and print count of word in string for word_occurrences.py

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -9,3 +9,6 @@ string = str(input("Enter a string of words: "))
 word_to_count = {}
 for word in string.split(" "):
     word_to_count[word] = word_to_count.get(word, 0) + 1
+
+for word in sorted(word_to_count):
+    print(f"{word} : {word_to_count[word]}")
