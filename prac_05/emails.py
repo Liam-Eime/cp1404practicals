50ab9567f8421d3a51ea68a071f4a605c5610afd
Liam EIme <liameime@outlook.com>
2022-10-17 17:26:56 +1000

    Complete name extraction from user email in emails.py

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -11,7 +11,14 @@ def main():
     email = input("Email: ")
     while email != "":
         email_list = email.split("@")
-        print(email_list)
+        email_title = email_list[0].split(".")
+        try:
+            first_name = email_title[0].title()
+            last_name = email_title[1].title()
+            print(f"{first_name}, {last_name}")
+        except IndexError:
+            first_name = email_title[0].title()
+            print(f"{first_name}")
         email = input("Email: ")
 
 
