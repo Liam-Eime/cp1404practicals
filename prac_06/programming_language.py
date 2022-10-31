2f19e180a9dbcdf69caa315891b3e49792ab84b7
Liam EIme <liameime@outlook.com>
2022-10-31 19:16:47 +1000

    Test programming_language.py class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,24 @@
+"""
+CP1404 | Practical 06 - programming_language and languages  | Liam Eime
+Estimate: 20 minutes
+Actual:
+"""
+
+
+class ProgrammingLanguage:
+    """Represent programming language information"""
+
+    def __init__(self, name="", typing="", reflection=False, year=0):
+        """Initialise values"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        """Return programming language information"""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """Check if the typing is dynamic"""
+        return self.typing == "Dynamic"
