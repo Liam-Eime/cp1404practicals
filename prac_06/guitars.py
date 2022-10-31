e1aa77598ed65661d7614e8868f838440ce3c3b1
Liam EIme <liameime@outlook.com>
2022-10-31 20:11:26 +1000

    Initial commit of guitars.py

---
+++ prac_06/guitars.py
@@ -0,0 +1,8 @@
+"""
+CP1404 | Practical 06 - guitar  | Liam Eime
+Program to use Guitar class
+This program uses a list to store all user's guitars and print their details
+Estimate: 30 minutes
+Actual:
+"""
+
