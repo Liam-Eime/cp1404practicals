f7f12d2d7e8c21b485909f4f9dc2762d940e9932
Liam EIme <liameime@outlook.com>
2022-11-07 11:51:42 +1000

    Complete My Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -6,17 +6,34 @@ from prac_07.guitar import Guitar
 
 FILENAME = "guitars.csv"
 
-guitars = []
-with open(FILENAME, 'r') as in_file:
-    for line in in_file:
-        parts = line.strip().split(',')
-        name = parts[0]
-        year = int(parts[1])
-        cost = float(parts[2])
+
+def main():
+    """"""
+    guitars = []
+    with open(FILENAME, 'r') as in_file:
+        for line in in_file:
+            parts = line.strip().split(',')
+            name = parts[0]
+            year = int(parts[1])
+            cost = float(parts[2])
+            guitar = Guitar(name, year, cost)
+            guitars.append(guitar)
+
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        print(f"{name} ({year}) : ${cost:,.2f} added.")
         guitar = Guitar(name, year, cost)
         guitars.append(guitar)
+        name = input("Name: ")
 
-guitars.sort()
+    guitars.sort()
+    with open(FILENAME, 'w') as out_file:
+        for guitar in guitars:
+            print(guitar)
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
 
-for guitar in guitars:
-    print(guitar)
+
+if __name__ == '__main__':
+    main()
