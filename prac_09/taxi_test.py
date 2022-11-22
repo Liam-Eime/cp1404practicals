22ff7341f5173a87cf6b8bc0f8a208a9fa609af6
Liam EIme <liameime@outlook.com>
2022-11-22 12:57:10 +1000

    Modify taxi class to use class variable

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -4,7 +4,7 @@ CP1404 | Liam Eime | taxi test
 
 from prac_09.taxi import Taxi
 
-my_taxi = Taxi("Prius 1", 100, 1.23)
+my_taxi = Taxi("Prius 1", 100)
 my_taxi.drive(40)
 print(my_taxi)
 print(f"Fare is ${my_taxi.get_fare()}")
