49ade5cd7428d9503cafd1a09f5748952a3de386
Liam EIme <liameime@outlook.com>
2022-11-22 14:11:00 +1000

    Add flagfall class variable

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -7,6 +7,7 @@ from prac_09.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """Silver service taxi that inherits from taxi class"""
+    flagfall = 4.50
 
     def __init__(self, name, fuel, fanciness):
         """Initialise a silver service taxi instance based on a taxi instance"""
