65f16a600b98a99243a7ae2ed6c0178f3ef3ac02
Liam EIme <liameime@outlook.com>
2022-11-22 14:29:54 +1000

    Test and complete silver service taxi exercise

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -2,3 +2,14 @@
 CP1404 | Liam Eime | Silver service taxi test
 """
 
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """Test silver service taxi class"""
+    silver_service_taxi = SilverServiceTaxi("Silver service taxi", 100, 2)
+    print(f"{silver_service_taxi.name} drove {silver_service_taxi.drive(18)}km"
+          f" with a fare of ${silver_service_taxi.get_fare()}")
+
+
+main()
